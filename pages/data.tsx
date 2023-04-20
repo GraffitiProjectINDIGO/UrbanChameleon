6c6ac6ac0b601af4852c98bb262fd0c79a003fb7
jonaschlegel <jona.schlegel@gmail.com>
2023-04-20 17:36:15 +0200

    Add image carusel and table

--- pages/data.tsx
+++ pages/data.tsx
@@ -1,6 +1,6 @@
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridCellParams, GridColDef } from '@mui/x-data-grid';
 import { Inter } from 'next/font/google';
 import React from 'react';
 import { Artifact, getArtifactsData } from '../components/api';
@@ -14,7 +14,7 @@ interface TablePageProps {
   artifacts: Artifact[];
 }
 
-const columns = [
+const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 90 },
   { field: 'title', headerName: 'Title', width: 150 },
   { field: 'description', headerName: 'Description', width: 200 },
@@ -23,10 +23,10 @@ const columns = [
     field: 'imageUrl',
     headerName: 'Depiction',
     width: 150,
-    renderCell: (params) => (
+    renderCell: (params: GridCellParams) => (
       <img
         src={`${params.value}?image_size=table`}
-        alt={params.row.title}
+        alt={params.row.title as string}
         style={{ width: '100%' }}
       />
     ),
@@ -37,7 +37,7 @@ const columns = [
     field: 'details',
     headerName: 'Details',
     width: 150,
-    renderCell: (params) => (
+    renderCell: (params: GridCellParams) => (
       <Tooltip title="Details">
         <IconButton onClick={() => console.log('View artifact', params.row.id)}>
           <EyeIcon size={20} fill="#979797" />
@@ -47,22 +47,24 @@ const columns = [
   },
 ];
 
-const TablePage = ({ artifacts }) => {
+const TablePage: React.FC<TablePageProps> = ({ artifacts }) => {
   if (!artifacts || artifacts.length === 0) {
     return <p>Loading graffiti data...</p>;
   }
 
   return (
     <>
+      <Navbar />
       <h1>Table Page</h1>
       <div style={{ height: 400, width: '100%' }}>
         <DataGrid
           rows={artifacts}
           columns={columns}
           disableColumnMenu
-          disableSelectionOnClick
+          disableRowSelectionOnClick
         />
       </div>
+      <Footer />
     </>
   );
 };
