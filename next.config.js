3d2115ce4191bb475d9f588fd2f7912211218121
Jona Schlegel <38097631+jonaschlegel@users.noreply.github.com>
2023-08-11 07:58:29 +0000

    Update styled components

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const withTM = require('next-transpile-modules')(['react-leaflet-cluster']);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  compiler: {
+    styledComponents: true,
+  },
+
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
     config.resolve.fallback = { fs: false };
