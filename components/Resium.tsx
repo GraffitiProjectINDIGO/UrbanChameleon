783a1e5055b75cbadd64de638df35af5ee160138
jonaschlegel <jona.schlegel@gmail.com>
2023-08-24 10:45:17 +0200

    Add 3D model from Cesium ion

--- components/Resium.tsx
+++ components/Resium.tsx
@@ -1,18 +1,33 @@
 import './Resium.module.scss';
-import { Cartesian3 } from 'cesium';
-import React, { useEffect, useState } from 'react';
+import { Cartesian3, Cesium3DTileset, Ion } from 'cesium';
+import React, { useEffect, useRef, useState } from 'react';
 import { Entity, Viewer } from 'resium';
+
+require('dotenv').config();
 
 export default function Resium() {
   const [showViewer, setShowViewer] = useState(false);
+  const viewerRef = useRef<any>(null);
 
   useEffect(() => {
     setShowViewer(true);
     return () => setShowViewer(false);
   }, []);
 
+  useEffect(() => {
+    if (viewerRef.current && viewerRef.current.cesiumElement) {
+      const viewer = viewerRef.current.cesiumElement;
+
+      if (process.env.CESIUM_ACCESS_TOKEN) {
+        Ion.defaultAccessToken = process.env.CESIUM_ACCESS_TOKEN;
+      } else {
+        console.error('CESIUM_ACCESS_TOKEN is not set in .env');
+      }
+    }
+  }, [viewerRef]);
+
   return showViewer ? (
-    <Viewer className="viewer-container">
+    <Viewer className="viewer-container" ref={viewerRef}>
       <Entity
         name="Vienna"
         position={Cartesian3.fromDegrees(16.3798, 48.2167, 100)}
