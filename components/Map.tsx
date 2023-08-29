3e902c14a6278769057d4b9e63413be8f2db317a
Jona Schlegel <jona.schlegel@gmail.com>
2023-08-29 12:14:04 +0200

    Add graffiti type based marker colouring in the map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,10 +1,11 @@
 import 'leaflet/dist/leaflet.css';
 import 'node_modules/react-leaflet-cluster/lib/assets/MarkerCluster.Default.css';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import L, { Icon, point } from 'leaflet';
 import dynamic from 'next/dynamic';
 import React, { useEffect, useState } from 'react';
+import ReactDOMServer from 'react-dom/server';
 import {
   LayersControl,
   MapContainer,
@@ -14,10 +15,11 @@ import {
   useMap,
 } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster';
-import { Artifact } from './api';
+import { Artifact, getArtifactsData } from './api';
 import Backdrop from './Blackdrop';
 import styles from './Map.module.scss';
 
+// Dynamic import for GraffitoOverlay
 const GraffitoOverlay = dynamic(() => import('./GraffitoOverlay'), {
   ssr: false,
   loading: () => <p>Loading...</p>,
@@ -27,17 +29,26 @@ interface MapProps {
   artifacts: Artifact[];
 }
 
-const customIcon = new Icon({
-  iconUrl: 'https://cdn-icons-png.flaticon.com/512/447/447031.png',
-  iconSize: [38, 38],
-});
-
+// Function to create custom cluster icons
 const createClusterCustomIcon: (cluster: any) => L.DivIcon = (cluster) => {
   return L.divIcon({
     html: `<div style="background-image: linear-gradient(135deg, #e95095, #7049ba); height: 2em; width: 2em; color: #fff; display: flex; align-items: center; justify-content: center; border-radius: 50%; font-size: 1.2rem; box-shadow: 0 0 0px 5px #fff;"><span>${cluster.getChildCount()}</span></div>`,
     className: 'custom-marker-cluster',
     iconSize: point(33, 33, true),
   });
+};
+
+// Function to group artifacts by type
+const groupArtifactsByType = (artifacts: Artifact[]) => {
+  const grouped: { [key: string]: Artifact[] } = {};
+  artifacts.forEach((artifact) => {
+    const type = artifact.types || 'Other Graffiti Types';
+    if (!grouped[type]) {
+      grouped[type] = [];
+    }
+    grouped[type].push(artifact);
+  });
+  return grouped;
 };
 
 interface MapPaneProps {
@@ -104,14 +115,79 @@ const MapPane: React.FC<MapPaneProps> = ({ isOverlayOpen }) => {
 };
 
 const Map: React.FC<MapProps> = ({ artifacts = [] }) => {
-  const [MarkerIcon, setMarkerIcon] = useState<L.Icon>();
+  const [markerIcon, setMarkerIcon] = useState<Icon | null>(null);
+  const [typeColorMapping, setTypeColorMapping] = useState<{
+    [key: string]: string;
+  }>({});
+
+  // Predefined colors
+  const predefinedColors = [
+    'rgb(39, 0, 137)', // indigo
+    'rgb(241, 136, 31)', // orange
+    'rgb(13, 172, 229)', // cyan
+    'rgb(210, 20, 92)', // pink
+    '#e95095', // violet
+    '#7049ba', // lila
+  ];
+
+  // Initialize type-color mapping
+  useEffect(() => {
+    const newTypeColorMapping: { [key: string]: string } = {};
+    let colorIndex = 0;
+
+    artifacts.forEach((artifact) => {
+      const type = artifact.types || 'Other Graffiti Types';
+      if (!newTypeColorMapping[type]) {
+        newTypeColorMapping[type] = predefinedColors[colorIndex];
+        colorIndex = (colorIndex + 1) % predefinedColors.length;
+      }
+    });
+
+    // Default color
+    newTypeColorMapping['Other Graffiti Types'] = 'gray';
+
+    setTypeColorMapping(newTypeColorMapping);
+  }, [artifacts]);
+
+  useEffect(() => {
+    const overlays = document.querySelectorAll(
+      '.leaflet-control-layers-overlays label',
+    );
+    overlays.forEach((labelElement) => {
+      const type = (labelElement as HTMLElement).textContent?.trim();
+      if (type) {
+        const color = typeColorMapping[type] || 'gray';
+        (labelElement as HTMLElement).classList.add('leaflet-custom-checkbox');
+        (labelElement as HTMLElement).style.setProperty(
+          '--checkbox-color',
+          color,
+        );
+      }
+    });
+  }, [typeColorMapping]);
+
+  // Function to generate SVG icon based on artifact type
+  const generateSVGIcon = (artifactType: string) => {
+    const color = typeColorMapping[artifactType] || 'gray';
+    const icon = <FontAwesomeIcon icon={faLocationDot} color={color} />;
+    return L.divIcon({
+      html: `<div class="leaflet-fa-icon">${ReactDOMServer.renderToString(
+        icon,
+      )}</div>`,
+      className: 'custom-icon',
+      iconSize: [80, 100],
+      iconAnchor: [0, 0],
+      popupAnchor: [0, 0],
+      tooltipAnchor: [0, 0],
+    });
+  };
 
   useEffect(() => {
     import('leaflet').then((L) => {
       const markerIcon = L.icon({
         iconUrl:
           'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-        iconSize: [25, 41],
+        iconSize: [80, 100],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
         tooltipAnchor: [16, -28],
@@ -140,6 +216,8 @@ const Map: React.FC<MapProps> = ({ artifacts = [] }) => {
     setSelectedGraffito(null);
   };
 
+  const groupedArtifacts = groupArtifactsByType(artifacts);
+
   return (
     <MapContainer
       doubleClickZoom={true}
@@ -152,8 +230,22 @@ const Map: React.FC<MapProps> = ({ artifacts = [] }) => {
       }`}
     >
       <div className={styles.mapWrapper}>
+        {/* Legend for marker colors */}
+        <div className={styles.legend}>
+          {Object.keys(typeColorMapping).map((type) => (
+            <div key={type}>
+              <span
+                className="checkmark"
+                style={{ color: typeColorMapping[type] }}
+              >
+                ✓
+              </span>{' '}
+              {type}
+            </div>
+          ))}
+        </div>
         <MapPane isOverlayOpen={isOverlayOpen} />
-        <LayersControl collapsed={false}>
+        <LayersControl collapsed={true}>
           <LayersControl.BaseLayer checked name="Watercolor">
             <TileLayer
               attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -172,39 +264,45 @@ const Map: React.FC<MapProps> = ({ artifacts = [] }) => {
               attribution="Tiles"
             />
           </LayersControl.BaseLayer>
-          <LayersControl.Overlay checked name="Graffiti marker">
-            <MarkerClusterGroup iconCreateFunction={createClusterCustomIcon}>
-              {artifacts.map((artifact) => {
-                if (artifact.latitude !== null && artifact.longitude !== null) {
-                  return (
-                    <Marker
-                      key={artifact.id}
-                      position={[artifact.latitude, artifact.longitude]}
-                      icon={customIcon}
-                    >
-                      <Popup>
-                        <h2 className="text-xl font-bold mb-4 whitespace-nowrap overflow-hidden overflow-ellipsis">
-                          {artifact.title ?? ''}
-                        </h2>
-                        <img
-                          src={artifact.imageUrl}
-                          alt={artifact.title}
-                          className="w-full h-auto rounded-md mb-4"
-                        />
-                        <button
-                          onClick={() => openOverlay(artifact)}
-                          className="bg-gradient-to-r from-e95095 to-7049ba text-white px-4 py-2 rounded-md mx-auto block z-10"
-                        >
-                          Graffito details
-                        </button>
-                      </Popup>
-                    </Marker>
-                  );
-                }
-                return null;
-              })}
-            </MarkerClusterGroup>
-          </LayersControl.Overlay>
+          {Object.keys(groupedArtifacts).map((type) => (
+            <LayersControl.Overlay checked name={type} key={type}>
+              <MarkerClusterGroup iconCreateFunction={createClusterCustomIcon}>
+                {groupedArtifacts[type].map((artifact) => {
+                  const customIcon = generateSVGIcon(type);
+                  if (
+                    artifact.latitude !== null &&
+                    artifact.longitude !== null
+                  ) {
+                    return (
+                      <Marker
+                        key={artifact.id}
+                        position={[artifact.latitude, artifact.longitude]}
+                        icon={customIcon}
+                      >
+                        <Popup>
+                          <h2 className="text-xl font-bold mb-4 whitespace-nowrap overflow-hidden overflow-ellipsis">
+                            {artifact.title ?? ''}
+                          </h2>
+                          <img
+                            src={artifact.imageUrl}
+                            alt={artifact.title}
+                            className="w-full h-auto rounded-md mb-4"
+                          />
+                          <button
+                            onClick={() => openOverlay(artifact)}
+                            className="bg-gradient-to-r from-e95095 to-7049ba text-white px-4 py-2 rounded-md mx-auto block z-10"
+                          >
+                            Graffito details
+                          </button>
+                        </Popup>
+                      </Marker>
+                    );
+                  }
+                  return null;
+                })}
+              </MarkerClusterGroup>
+            </LayersControl.Overlay>
+          ))}
         </LayersControl>
         {isOverlayOpen && selectedGraffito && (
           <Backdrop onClick={closeOverlay} />
